be07f0ed407c3656835fd4d004197ed6f62aaee8
Vladimir Ermakov <vooon341@gmail.com>
2025-07-24 17:35:16 +0200

    pbs-helper: works

--- pbs-helper/files/pbs.sh
+++ pbs-helper/files/pbs.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 
 . /usr/share/libubox/jshn.sh
-. /data/pbs.env
 
-local backup_log=/tmp/pbs-backup.log
+backup_log=/tmp/pbs-backup.log
+if [ -z "$HOSTNAME" ]; then
+	export HOSTNAME="$(uname -n)"
+fi
+
+. /etc/pbs.env
 
 proxmox-backup-client backup --ns "$PBS_NAMESPACE" "$PBS_BACKUP_NAME.pxar:/" 2>&1 | tee $backup_log
 rc=$?
 
-if [ $rc -ge 0 ]; then
+if [ $rc -gt 0 ]; then
 	stat="failed rc=$rc"
+	prio=9
 else
 	stat="successful"
+	prio=1
 fi
 
 if [ -z "$GOTIFY_URL" ]; then
@@ -21,10 +27,11 @@ fi
 json_init
 json_add_string title "$HOSTNAME backup: $stat"
 json_add_string message "$(cat $backup_log)"
+json_add_int priotity $prio
 json_close_object
 
 json_dump | curl -v "$GOTIFY_URL/message" -H "X-Gotify-Key: $GOTIFY_APP_TOKEN" -H "Content-Type: application/json" -X POST --data @-
 rc2=$?
 
-[ $rc -ge 0 ] && exit $rc
+[ $rc -gt 0 ] && exit $rc
 exit $rc2
