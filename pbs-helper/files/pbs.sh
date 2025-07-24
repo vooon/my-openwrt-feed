750703cabb71366c5d89b08b414e074a51d1cd44
Vladimir Ermakov <vooon341@gmail.com>
2025-07-24 17:10:00 +0200

    pbs-helper: notify via gotify

--- pbs-helper/files/pbs.sh
+++ pbs-helper/files/pbs.sh
@@ -8,10 +8,14 @@ local backup_log=/tmp/pbs-backup.log
 proxmox-backup-client backup --ns "$PBS_NAMESPACE" "$PBS_BACKUP_NAME.pxar:/" 2>&1 | tee $backup_log
 rc=$?
 
-if [ "$rc" -ge 0 ]; then
+if [ $rc -ge 0 ]; then
 	stat="failed rc=$rc"
 else
 	stat="successful"
+fi
+
+if [ -z "$GOTIFY_URL" ]; then
+	exit $rc
 fi
 
 json_init
@@ -19,6 +23,8 @@ json_add_string title "$HOSTNAME backup: $stat"
 json_add_string message "$(cat $backup_log)"
 json_close_object
 
-json_dump
+json_dump | curl -v "$GOTIFY_URL/message" -H "X-Gotify-Key: $GOTIFY_APP_TOKEN" -H "Content-Type: application/json" -X POST --data @-
+rc2=$?
 
-# todo call gotify
+[ $rc -ge 0 ] && exit $rc
+exit $rc2
