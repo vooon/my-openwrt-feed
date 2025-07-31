242e9364e030053cbc5496fdda957ee7dd074527
Vladimir Ermakov <vooon341@gmail.com>
2025-07-31 10:18:57 +0200

    pbs-helper: fix backup failures, notification

--- pbs-helper/files/pbs.sh
+++ pbs-helper/files/pbs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-. /usr/share/libubox/jshn.sh
+set -o pipefail
 
 backup_log=/tmp/pbs-backup.log
 if [ -z "$HOSTNAME" ]; then
@@ -12,6 +12,10 @@ fi
 proxmox-backup-client backup --ns "$PBS_NAMESPACE" "$PBS_BACKUP_NAME.pxar:/" "$@" 2>&1 | tee $backup_log
 rc=$?
 
+if [ -z "$GOTIFY_URL" ]; then
+	exit $rc
+fi
+
 if [ $rc -gt 0 ]; then
 	stat="failed rc=$rc"
 	prio=9
@@ -20,17 +24,17 @@ else
 	prio=1
 fi
 
-if [ -z "$GOTIFY_URL" ]; then
-	exit $rc
-fi
+# . /usr/share/libubox/jshn.sh
+# json_int
+# json_add_string title "$HOSTNAME backup: $stat"
+# json_add_string message "$(cat $backup_log)"
+# json_add_int priotity $prio
+# json_close_object
 
-json_init
-json_add_string title "$HOSTNAME backup: $stat"
-json_add_string message "\`\`\`\n$(cat $backup_log)\n\`\`\`"
-json_add_int priotity $prio
-json_close_object
-
-json_dump | curl -v "$GOTIFY_URL/message" -H "X-Gotify-Key: $GOTIFY_APP_TOKEN" -H "Content-Type: application/json" -X POST --data @-
+curl "$GOTIFY_URL/message" -H "X-Gotify-Key: $GOTIFY_APP_TOKEN" -X POST \
+	-F "title=$HOSTNAME backup: $stat" \
+	-F "message=\`\`\`\n$(cat $backup_log)\n\`\`\`" \
+	-F "priotity=$prio"
 rc2=$?
 
 [ $rc -gt 0 ] && exit $rc
