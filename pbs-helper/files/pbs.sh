11cb4f668a16dd53cd8cb1647f392c6d67bdf493
Vladimir Ermakov <vooon341@gmail.com>
2025-07-31 12:09:06 +0200

    pbs-helper: allow to run multiple backups
    Signed-off-by: Vladimir Ermakov <vooon341@gmail.com>

--- pbs-helper/files/pbs.sh
+++ pbs-helper/files/pbs.sh
@@ -2,21 +2,29 @@
 
 set -o pipefail
 
-backup_log=/tmp/pbs-backup.log
+backup_log="$(mktemp /tmp/pbs-backup.XXXX.log)"
+atexit() {
+	rm -f "$backup_log"
+}
+trap atexit EXIT
+
 if [ -z "$HOSTNAME" ]; then
 	export HOSTNAME="$(uname -n)"
 fi
 
 . /etc/pbs.env
 
-proxmox-backup-client backup --ns "$PBS_NAMESPACE" "$PBS_BACKUP_NAME.pxar:/" "$@" 2>&1 | tee $backup_log
+: "${PBS_BACKUP_NAME:=rootfs}"
+: "${PBS_PREFIX:=/}"
+
+proxmox-backup-client backup --ns "$PBS_NAMESPACE" "${PBS_BACKUP_NAME}.pxar:${PBS_PREFIX}" "$@" 2>&1 | tee "$backup_log"
 rc=$?
 
 if [ -z "$GOTIFY_URL" ]; then
 	exit $rc
 fi
 
-if [ $rc -gt 0 ]; then
+if [ $rc -ne 0 ]; then
 	stat="failed rc=$rc"
 	prio=9
 else
@@ -32,10 +40,10 @@ fi
 # json_close_object
 
 curl "$GOTIFY_URL/message" -H "X-Gotify-Key: $GOTIFY_APP_TOKEN" -X POST \
-	-F "title=$HOSTNAME backup: $stat" \
+	-F "title=$HOSTNAME $PBS_BACKUP_NAME backup: $stat" \
 	-F "$(printf 'message=```\n%s\n```\n' "$(cat $backup_log)")" \
 	-F "priority=$prio"
 rc2=$?
 
-[ $rc -gt 0 ] && exit $rc
+[ $rc -ne 0 ] && exit $rc
 exit $rc2
