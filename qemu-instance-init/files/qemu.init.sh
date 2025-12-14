bcc452e7c8c7ba841890d4847d84e95967572851
Vladimir Ermakov <vooon341@gmail.com>
2025-12-14 12:11:32 +0100

    qemu: simple start complete

--- qemu-instance-init/files/qemu.init.sh
+++ qemu-instance-init/files/qemu.init.sh
@@ -4,7 +4,14 @@ USE_PROCD=1
 START=99
 STOP=1
 
-extra_command "backup" "run proxmox-backup for this vm"
+# XXX TODO: detect arch
+PROG=/usr/bin/qemu-system-x86_64
+
+extra_command "backup" "run proxmox-backup-client"
+
+list_append_args() {
+	procd_append_param command "$1"
+}
 
 start_instance() {
 	local section=$1
@@ -12,23 +19,53 @@ start_instance() {
 	config_get_bool enabled "$section" enabled 1
 	[ "$enabled" = 1 ] || return
 
-	local vcpus ram cpu_type uuid qmp_port
+	local vcpus ram cpu_type uuid vnc qmp_port term_timeout
+	local root_disk root_disk_type
+	local net0_mac net0_bridge
 	config_get vcpus "$section" vcpus 1
-	config_get ram "$section" ram "512M"
-	config_get cpu_type "$section" cpu_type "host"
+	config_get ram "$section" ram '512M'
+	config_get cpu_type "$section" cpu_type 'host'
 	config_get uuid "$section" uuid
+	config_get vnc "$section" vnc 'none'
 	config_get qmp_port "$section" qmp_port 4444
-	config_get term_timeout "$section" 300
+	config_get term_timeout "$section" term_timeout 300
+	config_get root_disk "$section" root_disk
+	config_get root_disk_type "$section" root_disk_type 'qcow2'
+	config_get net0_mac "$section" net0_mac
+	config_get net0_bridge "$section" net0_bridge 'br-lan'
 
 	if [ -z "$uuid" ]; then
-		uuid=$(uuidgen -m --parent 5d762436-dc3c-45c4-9896-d078210eec5a --name "$section")
-		log warning "UUID is not set. Generated UUID5 from instance name: $section = $uuid"
+		uuid=$(uuidgen --md5 --namespace 5d762436-dc3c-45c4-9896-d078210eec5a --name "$section")
+		echo "WARN:$section: uuid is not set. Generated UUID5 from instance name: $uuid"
+	fi
+
+	if [ ! -e "$root_disk" ]; then
+		echo "ERROR:$section: root disk is not present: $root_disk"
+		return
 	fi
 
 	procd_open_instance "$section"
-	# XXX TODO: detect arch
-	procd_set_param command qemu-system-x86_64 -enable-kvm -cpu "$cpu_type" \
+	procd_set_param command "$PROG" -enable-kvm -cpu "$cpu_type" \
 		-smp "$vcpus" -m "$ram" -uuid "$uuid"
+
+	if [ "$root_disk_type" = "qemu2" ]; then
+		procd_append_param command -device virtio-scsi-pci,id=scsi
+		# TODO
+	fi
+	if [ "$root_disk_type" = "lvm" ]; then
+		procd_append_param command -device virtio-scsi-pci,id=scsi
+		procd_append_param command -drive "id=root-img,format=raw,file=$root_disk,if=virtio,cache.direct=on"
+	fi
+
+	if [ -n "$net0_mac" ]; then
+		procd_append_param command -device "virtio-net-pci,mac=$net0_mac,netdev=net0"
+		procd_append_param command -netdev "bridge,br=$net0_bridge,id=net0"
+	fi
+
+	[ -n "$vnc" ] && procd_append_param command -vnc "$vnc"
+	procd_append_param command -qmp "tcp:127.0.0.1:$qmp_port,server,nowait"
+
+	config_list_foreach "$instance" extra_args list_append_args
 
 	procd_set_param stdout 1
 	procd_set_param stderr 1
@@ -51,7 +88,9 @@ start_service() {
 }
 
 stop_service() {
-	:
+	echo "stop 1"
+	pgrep qemu
+	echo "stop 2"
 }
 
 
