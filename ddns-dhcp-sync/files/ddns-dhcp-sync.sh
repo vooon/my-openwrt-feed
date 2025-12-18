6d5d3926fbabbae5b53d72c378d4d0c8422299e8
Vladimir Ermakov <vooon341@gmail.com>
2025-12-18 11:14:20 +0100

    ddns-sync: filter by tag, fix multiple services

--- ddns-dhcp-sync/files/ddns-dhcp-sync.sh
+++ ddns-dhcp-sync/files/ddns-dhcp-sync.sh
@@ -2,12 +2,15 @@
 # shellcheck shell=busybox
 
 LOG_TAG=$(basename "$0")
+enable_debug=0
 
 . /lib/functions.sh
 
 _log() {
 	local level="$1"
 	shift
+	# local pid=$(readlink /proc/self)  # NOTE: $$ remains the same for subshells
+	# logger -s -t "${LOG_TAG}[$$:$pid]" -p "daemon.$level" "$level:" "$@"
 	logger -s -t "${LOG_TAG}[$$]" -p "daemon.$level" "$level:" "$@"
 }
 
@@ -21,6 +24,7 @@ log_info() {
 	_log INFO "$@"
 }
 log_debug() {
+	[ "$enable_debug" -eq 0 ] && return
 	_log DEBUG "$@"
 }
 
@@ -28,11 +32,12 @@ sync_host() {
 	local host="$1"
 	local update_url="$2" username="$3" password="$4"
 	local origin="$5"
-	local dhcp_instance="$6"
+	local select_instance="$6" select_tag="$7"
 
-	local name ip instance dns
+	local name ip tag instance dns
 	config_get name "$host" name
 	config_get ip "$host" ip
+	config_get tag "$host" tag
 	config_get instance "$host" instance
 	config_get_bool dns "$host" dns 0
 
@@ -41,8 +46,13 @@ sync_host() {
 		return
 	fi
 
-	if [ -n "$dhcp_instance" ] && [ "$dhcp_instance" != "$instance" ]; then
+	if [ -n "$select_instance" ] && [ "$select_instance" != "$instance" ]; then
 		log_debug "filter by instance: $name"
+		return
+	fi
+
+	if [ -n "$select_tag" ] && ! list_contains tag "$select_tag"; then
+		log_debug "filter by tag: $name"
 		return
 	fi
 
@@ -58,23 +68,65 @@ sync_host() {
 	fi
 }
 
+_sync_service_task() {
+	config_load dhcp
+	config_foreach sync_host host "$1" "$2" "$3" "$4" "$5" "$6"
+}
+
 sync_service() {
 	local instance="$1"
 
-	local update_url username password origin dhcp_instance dhcp_instance
+	local enabled update_url username password origin dhcp_instance select_instance select_tag
+	config_get_bool enabled "$instance" enabled
 	config_get update_url "$instance" update_url
 	config_get username "$instance" username
 	config_get password "$instance" password
 	config_get origin "$instance" origin
-	config_get dhcp_instance "$instance" dhcp_instance
+	config_get select_instance "$instance" select_instance
+	config_get select_tag "$instance" select_tag
 
-	config_load dhcp
-	config_foreach sync_host host "$update_url" "$username" "$password" "$origin" "$dhcp_instance"
+	if [ "$enabled" -eq 0 ]; then
+		return
+	fi
+
+	log_info "syncing service: $instance"
+	# run in subinterpreter because function needs to load another config
+	( _sync_service_task "$update_url" "$username" "$password" "$origin" "$select_instance" "$select_tag" )
+}
+
+do_sync() {
+	config_load ddns-dhcp-sync
+	config_foreach sync_service service
+
+	wait
 }
 
 main() {
-	config_load ddns-dhcp-sync
-	config_foreach sync_service service
+	local noexit=0
+	while [ "$#" -gt 0 ]; do
+		case "$1" in
+			--noexit)
+				noexit=1
+				;;
+			--debug)
+				enable_debug=1
+				;;
+			*)
+				echo "unknown option: $1"
+				;;
+		esac
+		shift
+	done
+
+	# procd instance must be active to be notified by trigger
+	if [ "$noexit" -ne 0 ]; then
+		while true; do
+			do_sync
+			sleep 3600
+		done
+	else
+		do_sync
+	fi
 }
 
 main "$@"
