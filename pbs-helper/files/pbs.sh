6024aac998554279897b400bf30309e7af139510
Vladimir Ermakov <vooon341@gmail.com>
2026-02-18 11:23:50 +0100

    pbs-helper: updated to 0.2.0-2

--- pbs-helper/files/pbs.sh
+++ pbs-helper/files/pbs.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-backup_log="$(mktemp -u -t pbs-backup.log.XXXXXX)"
+backup_log="$(mktemp -t pbs-backup.log.XXXXXX)"
 atexit() {
 	rm -f "$backup_log"
 }
@@ -13,11 +13,16 @@ if [ -z "$HOSTNAME" ]; then
 fi
 
 . /etc/pbs.env
+. /usr/share/libubox/jshn.sh
 
 : "${PBS_BACKUP_NAME:=rootfs}"
 : "${PBS_PREFIX:=/}"
 
-proxmox-backup-client backup --ns "$PBS_NAMESPACE" "${PBS_BACKUP_NAME}.pxar:${PBS_PREFIX}" "$@" 2>&1 | tee "$backup_log"
+if [ -n "$PBS_NAMESPACE" ]; then
+	proxmox-backup-client backup --ns "$PBS_NAMESPACE" "${PBS_BACKUP_NAME}.pxar:${PBS_PREFIX}" "$@" 2>&1 | tee "$backup_log"
+else
+	proxmox-backup-client backup "${PBS_BACKUP_NAME}.pxar:${PBS_PREFIX}" "$@" 2>&1 | tee "$backup_log"
+fi
 rc=$?
 
 if [ -z "$GOTIFY_URL" ]; then
@@ -32,19 +37,24 @@ else
 	prio=1
 fi
 
-# . /usr/share/libubox/jshn.sh
-# json_int
-# json_add_string title "$HOSTNAME backup: $stat"
-# json_add_string message "$(cat $backup_log)"
-# json_add_int priotity $prio
-# json_close_object
+msg_md="$(printf '```\n%s\n```\n' "$(cat "$backup_log")")"
 
-curl "$GOTIFY_URL/message" -H "X-Gotify-Key: $GOTIFY_APP_TOKEN" -X POST \
-	-F "title=$HOSTNAME $PBS_BACKUP_NAME backup: $stat" \
-	-F "$(printf 'message=```\n%s\n```\n' "$(cat $backup_log)")" \
-	-F "priority=$prio" \
-	-F 'extras={"client::display": {"contentType": "text/markdown"}}'
+json_init
+json_add_string title "$HOSTNAME $PBS_BACKUP_NAME backup: $stat"
+json_add_string message "$msg_md"
+json_add_int priority "$prio"
+json_add_object extras
+json_add_object "client::display"
+json_add_string contentType "text/markdown"
+json_close_object
+json_close_object
+
+json_dump | curl -fsS "$GOTIFY_URL/message" \
+	-H "X-Gotify-Key: $GOTIFY_APP_TOKEN" \
+	-H "Content-Type: application/json" \
+	-X POST \
+	--data-binary @-
 rc2=$?
 
-[ $rc -ne 0 ] && exit $rc
+[ "$rc" -ne 0 ] && exit "$rc"
 exit $rc2
